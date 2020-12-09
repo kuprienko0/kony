a059c42f78df2dc4c836431980e66b21e04c6546
Kuprienko Viktoria <vikkytorry1@gmail.com>
2020-12-09 16:20:39 +0200

    HW 7 second commit

--- HW7/script.js
+++ HW7/script.js
@@ -1,17 +1,18 @@
 let example = [[1, 1, 2],
-                [1,2, 2],
+                [0,2, 2],
                 [2, 1, 2]];
 function ticTacToe (arr){
     const winPosition = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     let rez = 0;
     let board = arr.reduce((acc,curr)=>[...acc,...curr],[]);
-    if (board.includes(0)) return -1;
     winPosition.forEach(([a,b,c])=>{
         if (board[a] === board[b] && board[b] === board[c] ){
             rez = board[a] === 1 ? 1 : 2;
         };
     });
-    return rez;
+    if (rez == 1 || rez == 2  || rez == 0) return rez;
+    if (board.includes('0')) return -1;
+
 };
 let example2 = 'Yesterday, we bumped into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. ' +
     'The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. ' +
