dd1b0450ad4dfdbbb09f4e8098fed8e92d8c5a84
Kuprienko Viktoria <vikkytorry1@gmail.com>
2020-12-15 01:42:45 +0200

    Create LuckyTicket task

--- demo1/main.js
+++ demo1/main.js
@@ -1,9 +1,10 @@
 import chessBoard from "./src/task1.js";
 import envelops from "./src/task2.js";
-import numericalSequence from "./src/task6.js";
 import luckyTicket from "./src/task5.js";
+import numericalSequence from "./src/task6.js";
+
 
 console.log(chessBoard(10,10,'*'));
 console.log(envelops({width: 3, height: 4}, {width:5, height:6}));
-console.log(luckyTicket(1001, 1002));
+console.log(luckyTicket(100000, 110000));
 console.log(numericalSequence(20,10));
--- demo1/src/task5.js
+++ demo1/src/task5.js
@@ -5,10 +5,15 @@ export default function luckyTicket(min, max){
         winner: null,
     };
     for (let i = min; i<max; i++){
+        if (i===0) rez.easy++;
         if (i>1000) {
             const right = i.toString().split('').slice(i.toString().length-3).reduce((curr, next) => +curr + +next, 0);
-            console.log(right);
-        }
+            const left = i.toString().split('').slice(0, i.toString().length-3 ).reduce((curr, next) => +curr + +next, 0);
+            if (left === right) rez.easy++;
+        };
+        const {even,odd} = i.toString().split('').reduce((curr, next) => next % 2 ? {...curr, even: +curr.even + +next}  : {...curr, odd: +curr.odd + +next}, {even: 0, odd: 0});
+        if (even === odd) rez.hard++;
     };
-
+    rez.winner = (rez.easy === rez.hard) ? 'Draw' : (rez.easy > rez.hard) ? 'Easy method' : 'Hard method';
+    return rez;
 };
