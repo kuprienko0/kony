4d0f3d4b1ad88551de053559d490b5002b32dcbe
Kuprienko Viktoria <vikkytorry1@gmail.com>
2021-01-10 06:03:03 +0200

    changes

--- demo2/app/components/bot/model-bot.js
+++ demo2/app/components/bot/model-bot.js
@@ -1,13 +1,17 @@
 export default class ModelBot{
     get url(){
-        const token = '1530316377:AAFUD1RozKwSYJNX-XR3m9SQVwuWZl9sZpU';
-        const chatId = '385692306';
 
-        return `https://api.telegram.org/bot${ token }/sendMessage?chat_id=${ chatId }&parse_mode=MarkdownV2&text=`;
+
+        return `https://api.telegram.org/bot${ this.token }/sendMessage?chat_id=${ this.chatId }&parse_mode=MarkdownV2&text=`;
     }
 
     send = order => {
         fetch(`${ this.url }${ order }`);
+    }
+
+    initTokens = _ => {
+        this.token = localStorage.getItem('bot');
+        this.chatId = localStorage.getItem('chat');
     }
 
 }
